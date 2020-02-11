c0696f96b78c0e3a561544de9c763f96ed570cb0
AnStarseed98 <an.dtStarseed>
2020-02-11 17:53:41 +0700

    add feature pug

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('hello world!'));
+app.set('view engine', 'pug');
+app.set('views ', 'pug');
+
+app.get('/', (req, res) => {
+    return res.render('index', {
+        name: 'An'
+    })
+});
+app.get('/users', (req, res) => {
+    return res.render('users/index', {
+        users: [
+            { id : 1, name: 'An'},
+            { id : 2, name: 'Thu'},
+        ]
+    })
+});
 
 app.listen(port, () => console.log(`your port is ${port}`));
