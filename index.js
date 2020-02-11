8917d6b5e7df811e0d87350ccba1a62f15e0bbf1
AnStarseed98 <an.dtStarseed>
2020-02-11 18:49:55 +0700

    add new feature create and search

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+const users = [
+    { id : 1, name: 'An'},
+    { id : 2, name: 'Thu'},
+];
 
 app.set('view engine', 'pug');
 app.set('views ', 'pug');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     return res.render('index', {
@@ -12,11 +21,26 @@ app.get('/', (req, res) => {
 });
 app.get('/users', (req, res) => {
     return res.render('users/index', {
-        users: [
-            { id : 1, name: 'An'},
-            { id : 2, name: 'Thu'},
-        ]
+        users: users
     })
+});
+
+app.get('/users/search', (req, res) => {
+
+    let valSearch = req.query.name ? users.filter( item => item.name.toLowerCase().indexOf(req.query.name.toLowerCase()) !== -1) : users;
+
+    return res.render('users/index', {
+        users: valSearch
+    })
+});
+
+app.post('/users/create', (req, res) => {
+    users.push( req.body );
+
+    return res.redirect('/users');
+});
+app.get('/users/create', (req, res) => {
+    return res.render('users/create')
 });
 
 app.listen(port, () => console.log(`your port is ${port}`));
